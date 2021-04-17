ecd657b8198df84131d674c6a2ee00c6fd229893
rskladan <radek.skladanowski@yahoo.com>
2021-04-17 18:47:10 +0200

    Splitted files for main class and consoleColor class. Updated checking date format. Updated adding new task

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -6,20 +6,23 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.Date;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String tasks[][] = readFile("tasks.csv");
+        String[][] tasks = readFile("tasks.csv");
         Scanner scanner = new Scanner(System.in);
 
         String input;
@@ -42,19 +45,16 @@ public class Main {
                     System.out.println(ConsoleColors.YELLOW + "You chose option ADD");
                     System.out.print(ConsoleColors.RESET);
                     tasks = addTask(tasks);
-                    rightChoice = true;
                 }
                 case "remove" -> {
                     System.out.println(ConsoleColors.YELLOW + "You chose option REMOVE");
                     System.out.print(ConsoleColors.RESET);
                     tasks = removeTasks(tasks);
-                    rightChoice = true;
                 }
                 case "list" -> {
                     System.out.println(ConsoleColors.YELLOW + "You chose option LIST");
                     System.out.print(ConsoleColors.RESET);
                     listTasks(tasks);
-                    rightChoice = true;
                 }
                 case "exit" -> {
                     System.out.println(ConsoleColors.YELLOW + "You chose option EXIT");
@@ -73,10 +73,10 @@ public class Main {
 
     public static String[][] readFile(String fileName) {
         Path path1 = Paths.get(fileName);
-        String tasks[][] = new String[0][0];
+        String[][] tasks = new String[0][0];
         int count = 0;
 
-        if(!Files.exists(path1)) {
+        if (!Files.exists(path1)) {
             System.out.println("File does't exist");
             System.exit(0);
         }
@@ -106,16 +106,16 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         boolean doubleCheck = false;
         int input;
-        System.out.println("Please select the task's number you would like to remove. From 0 to " + (tasks.length-1));
+        System.out.println("Please select the task's number you would like to remove. From 0 to " + (tasks.length - 1));
 
-        while(!doubleCheck) {
-            if(!scan.hasNextInt()) {
+        while (!doubleCheck) {
+            if (!scan.hasNextInt()) {
                 scan.next();
-                System.out.println("Incorrect value. Please select the task from 0 to " + (tasks.length-1));
+                System.out.println("Incorrect value. Please select the task from 0 to " + (tasks.length - 1));
             }
             input = scan.nextInt();
             if (input < 0 || input >= tasks.length) {
-                System.out.println("Incorrect value. Please select the task from 0 to " + (tasks.length-1));
+                System.out.println("Incorrect value. Please select the task from 0 to " + (tasks.length - 1));
             } else {
                 System.out.println("Removed task number: " + input);
                 tasks = ArrayUtils.remove(tasks, input);
@@ -136,47 +136,51 @@ public class Main {
         }
     }
 
-    public static String[][] copyMultiArray(String[][] tasks) {
-        String [][] currentArr = new String[tasks.length+1][3];
-        for(int i=0; i<tasks.length; i++) {
-            System.arraycopy(tasks[i], 0, currentArr[i], 0, tasks[i].length);
-        }
-        return currentArr;
-    }
-
     public static String[][] addTask(String[][] tasks) {
         Scanner scan = new Scanner(System.in);
-        String[][] expandedArr = copyMultiArray(tasks);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        boolean validDate = false;
-        dateFormat.setLenient(validDate);
+        String[][] expandedArr = Arrays.copyOf(tasks, tasks.length + 1);
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+////        mozna wykorzystac tu localDate
+//        boolean validDate = false;
+//        dateFormat.setLenient(validDate);
+
+
+        System.out.println(expandedArr.length);
+        expandedArr[expandedArr.length - 1] = new String[3];
 
         System.out.println("Please add task description:");
         String task = scan.nextLine();
         System.out.println("Please add task due date. Please use format yyyy-mm-dd");
         String deadline = "";
 
+        DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        boolean validDate = false;
+
         while (!validDate) {
             deadline = scan.nextLine();
             try {
-                Date javaDate = dateFormat.parse(deadline);
-                validDate = true;
-            } catch (ParseException e) {
+                validDate = LocalDate.from(customFormatter.parse(deadline)).isAfter(LocalDate.now());
+                System.out.println(validDate);
+                if (!validDate) {
+                    System.out.println("Invalid date. Please try again");
+                    System.out.println("Today is " + LocalDate.now());
+                }
+            } catch (DateTimeParseException e) {
                 System.out.println("Invalid date. Please try again");
                 validDate = false;
             }
         }
 
         System.out.println("Is your task important? true/false");
-        while(!scan.hasNextBoolean()) {
+        while (!scan.hasNextBoolean()) {
             scan.next();
             System.out.println("Incorrect value. Is your task important? true/false");
         }
         boolean isImportant = scan.nextBoolean();
 
-        expandedArr[expandedArr.length-1][0] = task;
-        expandedArr[expandedArr.length-1][1] = deadline;
-        expandedArr[expandedArr.length-1][2] = Boolean.toString(isImportant);
+        expandedArr[expandedArr.length - 1][0] = task;
+        expandedArr[expandedArr.length - 1][1] = deadline;
+        expandedArr[expandedArr.length - 1][2] = Boolean.toString(isImportant);
 
         System.out.println(ConsoleColors.GREEN_BOLD + "Success! Task added" + ConsoleColors.RESET);
 
@@ -202,83 +206,6 @@ public class Main {
             e.printStackTrace();
         }
     }
-
-    public static class ConsoleColors {
-        // Reset
-        public static final String RESET = "\033[0m";  // Text Reset
-
-        // Regular Colors
-        public static final String BLACK = "\033[0;30m";   // BLACK
-        public static final String RED = "\033[0;31m";     // RED
-        public static final String GREEN = "\033[0;32m";   // GREEN
-        public static final String YELLOW = "\033[0;33m";  // YELLOW
-        public static final String BLUE = "\033[0;34m";    // BLUE
-        public static final String PURPLE = "\033[0;35m";  // PURPLE
-        public static final String CYAN = "\033[0;36m";    // CYAN
-        public static final String WHITE = "\033[0;37m";   // WHITE
-
-        // Bold
-        public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
-        public static final String RED_BOLD = "\033[1;31m";    // RED
-        public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
-        public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-        public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
-        public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
-        public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
-        public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
-
-        // Underline
-        public static final String BLACK_UNDERLINED = "\033[4;30m";  // BLACK
-        public static final String RED_UNDERLINED = "\033[4;31m";    // RED
-        public static final String GREEN_UNDERLINED = "\033[4;32m";  // GREEN
-        public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
-        public static final String BLUE_UNDERLINED = "\033[4;34m";   // BLUE
-        public static final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
-        public static final String CYAN_UNDERLINED = "\033[4;36m";   // CYAN
-        public static final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE
-
-        // Background
-        public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK
-        public static final String RED_BACKGROUND = "\033[41m";    // RED
-        public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
-        public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
-        public static final String BLUE_BACKGROUND = "\033[44m";   // BLUE
-        public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
-        public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
-        public static final String WHITE_BACKGROUND = "\033[47m";  // WHITE
-
-        // High Intensity
-        public static final String BLACK_BRIGHT = "\033[0;90m";  // BLACK
-        public static final String RED_BRIGHT = "\033[0;91m";    // RED
-        public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
-        public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
-        public static final String BLUE_BRIGHT = "\033[0;94m";   // BLUE
-        public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
-        public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
-        public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
-
-        // Bold High Intensity
-        public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
-        public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
-        public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
-        public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
-        public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";  // BLUE
-        public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
-        public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
-        public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
-
-        // High Intensity backgrounds
-        public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
-        public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
-        public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
-        public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
-        public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
-        public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
-        public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
-        public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
-    }
-
-
 }
 
 
